d8e294ef5326707029e62bfa78aaeb9bb534fc4e
darshanlal-sembun <darshanlalsembun@gmail.com>
2022-12-23 13:05:15 +0530

    only numbers allowed

---
+++ only_number_allow/js/script.js
@@ -0,0 +1,11 @@
+var number = document.getElementById("only_numbers");
+number.addEventListener("keypress",check);
+function check(event){
+    if((event.keyCode > 47) && (event.keyCode <= 57 )){
+        alert("Entered Number is : " + event.key + "\n" + " Number's keycode is : " + event.keyCode);
+    }
+    else{
+        event.preventDefault();
+        alert("Enter Only Numbers");
+    }
+};
